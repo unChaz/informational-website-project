852f6f2ea9114b187d7ed0ee38ec2679693ffbe4
Chaz Ferguson <chaz.ferguson@gmail.com>
2014-03-12 22:22:57 -0400

    added rails app

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+InformationalWebsiteProject::Application.config.session_store :cookie_store, key: '_informational-website-project_session'
